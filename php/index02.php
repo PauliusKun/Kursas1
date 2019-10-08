f2f1082e81c1f4b9ed836f1790ae66060205c80b
PauliusKun <p.kundrutas@gmail.com>
2019-10-08 10:48:53 +0300

    Pridėtas klasės darbas PHP02

---
+++ php/index02.php
@@ -0,0 +1,19 @@
+<?php
+		$country = 'Lietuva';
+		$city = 'Vilnius';
+		$population = 574221;
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP02</title>
+	</head>
+	<body>
+		<?php
+			echo $city.' '.'-'.' '.'Lietuvos sostinė'.'<br>';
+			echo '"'.$city.' '.'ir'.' '.$country.' '.'sparčiai auga'.'"';
+		?>
+	</body>
+</html>
