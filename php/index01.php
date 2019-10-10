62575d7a269fc655eea125e519906089a95c60a9
PauliusKun <p.kundrutas@gmail.com>
2019-10-10 10:50:17 +0300

    Pridėtas klasės darbas PHP01

--- php/index01.php
+++ php/index01.php
@@ -4,7 +4,7 @@
 	$population = 574221;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="lt">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
